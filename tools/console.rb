9e9449081f52c36d20ca2b529f4fe8976fe1d762
jisung594 <jisung594@gmail.com>
2019-01-04 10:24:51 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,17 @@
 require_relative '../config/environment.rb'
 
+herb = Artist.new("Herb Lubalin", 55)
+vincent = Artist.new("Vincent van Gogh", 30)
 
+g1 = Gallery.new("Museo Gallery", "Rome")
+g2 = Gallery.new("Gallerie Futura", "Boston")
+g3 = Gallery.new("Clarendon Collection", "Seattle")
 
+p1 = Painting.new("Negative in Black", 9800, herb, g3)
+p2 = Painting.new("Starry Night", 860000, vincent, g1)
+p3 = Painting.new("Sunflower Field", 780000, vincent, g1)
+p4 = Painting.new("Disaster in Paradise", 5000, vincent, g3)
+p5 = Painting.new("Calla Lily", 24000, vincent, g2)
 
 binding.pry
 
